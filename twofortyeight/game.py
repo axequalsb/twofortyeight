f19705f9c361fcb9b9ad73e74d9dbd1b2833f45b
Ax Equalsb <axequalsb@gmail.com>
2026-01-17 21:21:22 -0300

    WIP: Fixing the merge logic
    Still having issues when the merge happens at the end of the axis

--- twofortyeight/game.py
+++ twofortyeight/game.py
@@ -2,7 +2,6 @@ import asyncio
 import pygame
 import random
 import logging
-import sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -19,11 +18,13 @@ class Grid(pygame.sprite.Group):
 
     def get_empty_pos(self):
         """Get a random empty position in the grid."""
-        empty_positions = [(x, y) for y in range(self.n) for x in range(self.n) if self.grid[x][y] is None]
-        logger.debug(f"Empty positions: {len(empty_positions)}")
-        if not empty_positions:
-            return None
-        return random.choice(empty_positions)
+        empty_positions = [
+            (x, y) for y in range(self.n) for x in range(self.n) if self.grid[x][y] is None
+        ]
+        logger.debug("Empty positions: %s", len(empty_positions))
+        if empty_positions:
+            return random.choice(empty_positions)
+        return None
 
     def add(self, tile=None):
         """Add a tile to the grid."""
@@ -38,8 +39,23 @@ class Tile(pygame.sprite.Sprite):
     """A tile in the 2048 game."""
     def __init__(self, value, position=None, rect_size=0):
         super().__init__()
+
+        self.default_tiles = {
+            "2": f"{ASSETS}/00002.png",
+            "4": f"{ASSETS}/00004.png",
+            "8": f"{ASSETS}/00008.png",
+            "16": f"{ASSETS}/00016.png",
+            "32": f"{ASSETS}/00032.png",
+            "64": f"{ASSETS}/00064.png",
+            "128": f"{ASSETS}/00128.png",
+            "256": f"{ASSETS}/00256.png",
+            "512": f"{ASSETS}/00512.png",
+            "1024": f"{ASSETS}/01024.png",
+            "2048": f"{ASSETS}/02048.png",
+        }
+
         self.value = value
-        self.image = pygame.image.load(f"./0000{value}.png").convert_alpha()
+        self.image = pygame.image.load(self.default_tiles[str(value)]).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect_size = rect_size
 
@@ -48,51 +64,82 @@ class Tile(pygame.sprite.Sprite):
         self.rect.topleft = (col * rect_size, row * rect_size)
         self.coords = (row, col)
 
+
     def _new_coords(self, axis, index, current):
         """Calculate new coordinates for the tile based on movement direction.
 
         Returns the minimum empty index of this axis.
-        If there's no empty space, stay in the same position
+
+        * If there's no empty space, stay in the same position
+        * If a merge is possible, return the merged position and new value
         """
-        logger.debug(f"Axis: {axis}")
-        logger.debug(f"Index: {index}")
+        logger.debug("Axis: %s", axis)
+        logger.debug("Index: %s", index)
         move = False
         for i in index:
             if axis[i] is None:
                 move = True
                 break
         if move:
-            logger.debug(f"Moving from {current} to {i}")
-            return i, self.value
-        logger.debug(f"Staying at {current}")
-        return current, self.value
+            logger.debug("Moving from %s to %s", current, i)
+        else:
+            logger.debug("Staying at %s", current)
+            i = current
+        # Check if we can merge with the next tile (only if we are not currently
+        # at the last position in this axis)
+        logger.debug("Checking for merge at %s", i)
+        logger.debug(f"{list(index)=}")
+        if len(list(index)) > 0 and i - 1 >= 0 and axis[i - 1] == self.value:
+            logger.debug("Merging tile at %s with tile at %s", i, i-1)
+            self.value *= 2
+            return i - 1, self.value
+        return i, self.value
 
     def update(self, position=None):
         """Update the tile's position on the screen."""
-        if position is None:
-            return
-        else:
-            # Clear out current tile's position
-            logger.debug(f"Updating tile {self.value} at {self.coords} moving {position}")
+        if position:
+            logger.debug("Updating tile %s at %s moving %s", self.value, self.coords, position)
             game = self.groups()[0]
             if position == "up":
                 col = self.coords[1]
-                row, value = self._new_coords([game.grid[i][col] for i in range(game.n)], range(self.coords[0]), self.coords[0])
+                row, value = self._new_coords(
+                    [game.grid[i][col] for i in range(game.n)],
+                    range(self.coords[0]),
+                    self.coords[0]
+                )
             elif position == "down":
                 col = self.coords[1]
-                row, value = self._new_coords([game.grid[i][col] for i in range(game.n)], range(game.n-1, self.coords[0], -1), self.coords[0])
+                row, value = self._new_coords(
+                    [game.grid[i][col] for i in range(game.n)],
+                    range(game.n-1, self.coords[0], -1),
+                    self.coords[0]
+                )
             elif position == "left":
                 row = self.coords[0]
-                col, value = self._new_coords(game.grid[row][:], range(self.coords[1]), self.coords[1])
+                col, value = self._new_coords(
+                    game.grid[row][:],
+                    range(self.coords[1]),
+                    self.coords[1]
+                )
             elif position == "right":
                 row = self.coords[0]
-                col, value = self._new_coords(game.grid[row][:], range(game.n-1, self.coords[1], -1), self.coords[1])
-            # Set new position
+                col, value = self._new_coords(
+                    game.grid[row][:],
+                    range(game.n-1, self.coords[1], -1),
+                    self.coords[1]
+                )
+            else:
+                return
+
+            # Clear out current tile's position
             game.grid[self.coords[0]][self.coords[1]] = None
-            game.grid[row][col] = self.value
+            # Set new position
             self.rect.topleft = (col * self.rect_size, row * self.rect_size)
             self.coords = (row, col)
             self.value = value
+            self.image = pygame.image.load(self.default_tiles[str(self.value)]).convert_alpha()
+            # Update the grid
+            game.grid[row][col] = self.value
 
 
 def clear_screen(screen, n=5):
@@ -122,32 +169,22 @@ async def main(n=5):
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("2048 Game")
 
-    clock = pygame.time.Clock()
+    #clock = pygame.time.Clock()
     running = True
 
-    default_tiles = {
-        "2": "./00002.png",
-        "4": "./00004.png",
-        # "8": "./twofortyeight/assets/00008.png",
-        # "16": "./twofortyeight/assets/00016.png",
-        # "32": "./twofortyeight/assets/00032.png",
-        # "64": "./twofortyeight/assets/00064.png",
-        # "128": "./twofortyeight/assets/00128.png",
-        # "256": "./twofortyeight/assets/00256.png",
-        # "512": "./twofortyeight/assets/00512.png",
-        # "1024": "./twofortyeight/assets/01024.png",
-        # "2048": "./twofortyeight/assets/02048.png",
-    }
-
+    # Draw basic grid
     grid = Grid(n)
     rect_size = clear_screen(screen, n)
     grid.draw(screen)
+
+    # Spawn new tile at random position
     new_pos = grid.get_empty_pos()
     grid.add(Tile(2, new_pos, rect_size))
-    logger.debug(f"Added tile at position {new_pos}")
+    logger.debug("Added tile at position %s", new_pos)
     grid.tiles.append(grid.sprites()[-1])
     screen.blit(grid.tiles[-1].image, (grid.tiles[-1].rect.x, grid.tiles[-1].rect.y))
     logger.debug(grid.grid)
+
     while running:
         # fill the screen with a color to wipe away anything from last frame
         for event in pygame.event.get():
@@ -168,13 +205,16 @@ async def main(n=5):
                 # Spawn a new tile at a random position in the grid
                 # If no empty positions are available, the game is over
                 new_pos = grid.get_empty_pos()
-                logger.debug(f"New position for tile: {new_pos}")
+                logger.debug("New position for tile: %s", new_pos)
                 if new_pos is None:
                     logger.debug("Game Over!")
+                    pygame.display.set_caption("Game Over!")
+                    if IN_PYSCRIPT:
+                        page["h3#game-over"].innerHTML = "Game Over!"
                     running = False
                 else:
                     grid.add(Tile(2, new_pos, rect_size))
-                    logger.debug(f"Added tile at position {new_pos}")
+                    logger.debug("Added tile at position %s", new_pos)
                     grid.tiles.append(grid.sprites()[-1])
                 rect_size = clear_screen(screen, n)
                 grid.draw(screen)
@@ -192,6 +232,12 @@ async def main(n=5):
 
 try:
     asyncio.get_running_loop()
-    asyncio.create_task(main(5))
+    IN_PYSCRIPT = True
+    ASSETS = "."
+    from pyscript.web import page
+    asyncio.create_task(main(3))
 except RuntimeError:
-    asyncio.run(main(5))
+    IN_PYSCRIPT = False
+    ASSETS = "./twofortyeight/assets"
+    asyncio.run(main(3))
+
--- twofortyeight/index.md
+++ twofortyeight/index.md
@@ -1,6 +1,8 @@
 # 2048
 
-High score: 0
+<h4 id="high-score">High score: 0</h3>
+<h3 id="score">Score: 0</h3>
+<h3 id="game-over"> </h3>
 
-<canvas id="canvas"></canvas>
+<canvas class="game-canvas" id="canvas"></canvas>
 <script type="py-game" src="./game.py" config="./pyscript.toml"></script>
